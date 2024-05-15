7ed5038ed2d5b8f7fe151dcbf88540be5ce3f986
Mielke Piotr <piotr.mielke@atena.pl>
2024-05-15 14:56:15 +0200

    Add types

---
+++ types.js
@@ -0,0 +1,43 @@
+// Number 
+const number = 24;
+const number2 = 27.54;
+
+//console.log(number - number2);
+
+// String
+const firstName = "John";
+const animal = "Dog";
+
+//console.log(firstName + " has a new animal, which is: " + animal)
+
+// Logical type
+const woman = false;
+const man = true;
+
+//console.log(typeof(number))
+
+// Null 
+const color = null;
+
+//console.log(color)
+
+// Tables (Object)
+const exampleTable = ["Name1", "Name2", "Name3", 2, true];
+
+// Object
+const person = {
+    name: "John",
+    age: 29,
+    colorEye: "green"
+}
+
+//console.log(person.age);
+
+// Fun with types 
+//console.log(person.age.length) // check the length
+//console.log(exampleTable[1].toUpperCase())
+
+const resultsOfSum = number + animal;
+
+console.log(number2.toFixed(5)); 
+console.log(typeOf(String(number)));
